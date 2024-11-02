0e856de89d80045c266532fd021efa48dc4b74f9
RamaKrishna3008 <148173284+RamaKrishna3008@users.noreply.github.com>
2024-11-02 16:20:58 +0530

    Delete ERP directory

--- ERP/src/main/java/com/klef/jfsd/sdp/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.klef.jfsd.sdp.service;
-
-import com.klef.jfsd.sdp.model.Student;
-
-public interface StudentService{
-	public Student studentLogin(long id, String password);
-	public boolean updateStudent(Student st);
-
-}
